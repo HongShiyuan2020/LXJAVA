01b680780f520d6b295a7c5e4736d602972ac34d
HongShiyuan2020 <1850456500@qq.com>
2020-11-08 12:48:51 +0800

    java

--- Test/src/LX1/LX1.java
+++ Test/src/LX1/LX1.java
@@ -3,6 +3,7 @@ package LX1;
 
 public class LX1 {
     public static void main(String[] args) {
-        System.out.println("hongshiyyuan");        
+        System.out.println("hongshiyyuan");  
+              
     }
 }
